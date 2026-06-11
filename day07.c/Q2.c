f4a9fdbf56763c97c90748092d7aa49c4b8210cb
vivekyadav6779 <vy6799064@gmail.com>
2026-06-11 18:05:36 +0530

    Add files via upload

---
+++ day07.c/Q2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n;i;
+    printf("enter number of terms:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("%d",fib(i));
+
+    }
+    return 0;
+
+
+}
+int fib(int n){
+    if(n==0)
+    return 0;
+if(n==1)
+return1;
+return fib(n-1) + fib(n-2);
+}
+//logic behind the code 
+fibonacci series 
+0 1 1 2 3 5 8 13.....
+formula
+fib(0)=0
+fib(1)=1
+fib(n)=fib(n-1)+fib(n-20)
+
