9a981de7ac9dd57d170ffa25c12aaf6ac8df0dc9
vivekyadav6779 <vy6799064@gmail.com>
2026-06-30 16:11:38 +0530

    Add files via upload

---
+++ day19.c/Q3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &r, &c);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Transpose of the matrix:\n");
+    for(i = 0; i < c; i++) {
+        for(j = 0; j < r; j++)
+            printf("%d ", a[j][i]);
+        printf("\n");
+    }
+
+    return 0;
+}
