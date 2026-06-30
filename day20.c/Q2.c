c394e923fd4d48bf8262e679dbe8b1d12af16167
vivekyadav6779 <vy6799064@gmail.com>
2026-06-30 16:17:53 +0530

    Add files via upload

---
+++ day20.c/Q2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, flag = 1;
+
+    printf("Enter order of square matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag)
+        printf("Matrix is Symmetric");
+    else
+        printf("Matrix is Not Symmetric");
+
+    return 0;
+}
